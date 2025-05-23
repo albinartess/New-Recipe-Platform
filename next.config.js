ce0877e14dda82a0a44ccc29d741c1d02bca23ee
albinartess <albinartess@mail.ru>
2025-05-23 09:12:36 +0800

    Fix: Remove exportPathMap and add missing postcss package, recreate new recipe page

--- next.config.js
+++ next.config.js
@@ -13,14 +13,6 @@ const nextConfig = {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
-  // Отключаем статическую генерацию для страниц с ошибками
-  exportPathMap: async function () {
-    return {
-      '/': { page: '/' },
-      '/recipes': { page: '/recipes' },
-      '/recipes/new': { page: '/recipes/new' },
-    }
-  }
 }
 
 module.exports = nextConfig 
